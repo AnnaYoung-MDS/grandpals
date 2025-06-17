eadaa76dd938031b1bf8374f4c77849692f707d9
Anna Young <anna.young@mds.ac.nz>
2025-06-17 11:27:08 +1000

    Trying Database Again - still not working

---
+++ js/post.js
@@ -0,0 +1,3 @@
+import { createClient } from 'npm:@supabase/supabase-js@2'
+
+const supabase = createClient('https://dxwabfntywpqajoaxxqc.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImR4d2FiZm50eXdwcWFqb2F4eHFjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg0ODE1NDYsImV4cCI6MjA2NDA1NzU0Nn0.rePMQPZ3ynWt0_STsm9qjlfDcpJcQ8O7xmSQhoqQ0tM')
